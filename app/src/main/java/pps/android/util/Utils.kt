ddde21c3ea827871da2a60fdcfe231482dffbeff
Joel Dean <jdeanjj1000@gmail.com>
2018-04-30 12:49:40 -0500

    added dp to px vice versa utility methods.

---
+++ app/src/main/java/pps/android/util/Utils.kt
@@ -0,0 +1,16 @@
+package pps.android.util
+
+import android.content.Context
+import android.util.DisplayMetrics
+
+object Utils {
+    fun dpToPx(dp: Int, context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
+    }
+
+    fun pxToDp(px: Int, context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
+    }
+}
